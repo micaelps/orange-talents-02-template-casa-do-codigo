cbb421b19aa6b1dc8cc26b28800b8badc9867248
micaelps <3.1416micael@gmail.com>
2021-03-03 10:25:39 -0300

    removendo repository não utilizado

--- src/main/java/com/micaelps/casadocodigo/novacategoria/NovaCategoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.micaelps.casadocodigo.novacategoria;
-
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.Optional;
-
-public interface NovaCategoriaRepository extends CrudRepository<Categoria, Long> {
-    Optional<Categoria> findByNome(String nome);
-}
--- src/main/java/com/micaelps/casadocodigo/novoautor/AutorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.micaelps.casadocodigo.novoautor;
-
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.Optional;
-
-public interface AutorRepository extends CrudRepository<Autor, Long> {
-    Optional<Autor> findByEmail(String email);
-}
